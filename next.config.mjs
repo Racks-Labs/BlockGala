50da710c95fef4ca5feaa2da7f8fa4d4f3c7c748
alex-alra-arteaga <alexarteagacontijoch@gmail.com>
2023-10-06 19:48:54 +0200

    feat: move template into app dir

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-import "./src/env.mjs";
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  output: "standalone",
-};
-export default nextConfig;
